f9ca81197943b061ee8fa5e2d75f5f11f8a8cab2
Polina Katkova <39344446+thePolly@users.noreply.github.com>
2023-08-10 16:05:32 +0200

    Update images.js

--- src/constants/images.js
+++ src/constants/images.js
@@ -3,6 +3,7 @@ import me_bg from '../assets/me_bg.jpg';
 import me_bg2 from '../assets/me_bg2.png';
 import me_only from '../assets/me_only.png';
 import bg from '../assets/bg.jpg';
+import measkid from '../assets/measkid3.jpg';
 
 
 export default {
@@ -10,5 +11,6 @@ export default {
   me_bg2,
   me_bg,
   me_only,
-  bg
+  bg,
+  measkid
 }
