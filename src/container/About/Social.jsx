74842c1d39ff7ad8c79ad20edb43eaa0d52b296e
Polina Katkova <39344446+thePolly@users.noreply.github.com>
2025-02-25 17:53:38 +0100

    Update Social.jsx

--- src/container/About/Social.jsx
+++ src/container/About/Social.jsx
@@ -6,12 +6,12 @@ import {GrDribbble,GrGithub, GrLinkedinOption, GrLinkedin} from 'react-icons/gr'
 const Social = () => {
   return (
     <div className='home__social'>
-      <div className='icon-container'>
-      <a href='https://dribbble.com/sinelga/shots' className='home__social-icon' target="_blank">
-<GrDribbble ></GrDribbble>
-    </a>
-      </div>
 
+<div className='icon-container'>
+   <a href='https://github.com/thePolly' className='home__social-icon github' target="_blank">
+<GrGithub></GrGithub>
+</a>
+   </div>
   
 
    <div className='icon-container'>
