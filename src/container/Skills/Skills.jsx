c9f1ad8934f0b9d0aeeb425f84a48a3a8a40a64f
Polina Katkova <39344446+thePolly@users.noreply.github.com>
2025-11-09 10:49:30 +0100

    Refactor skills data and add infrastructure skills

--- src/container/Skills/Skills.jsx
+++ src/container/Skills/Skills.jsx
@@ -8,20 +8,10 @@ import SkillOverview from './SkillOverview';
 
 
 
-
-
-import { saveAs } from 'file-saver';
-
-
-import ReactVivus from 'react-vivus';
-
-import svg from './test1.svg';
-
-
 import {IoCodeSlashSharp,
   IoGameController,
   IoDesktopOutline,
-  IoLaptopOutline,
+  IoSettingsSharp,
   IoEyedropOutline,
   IoColorPalette,
   IoColorPaletteOutline} from 'react-icons/io5'
@@ -29,54 +19,24 @@ import {IoCodeSlashSharp,
 
 const backendSkills=  {
   name: "Backend",
-  list: ["Java", "Python", "Groovy", "Jenkins"],
+  list: ["Java", "Spring Boot", "Python", "SQL", "Groovy"],
  icon: {IoCodeSlashSharp}
 }
 const frontendSkills=  {
   name: "Frontend",
-  list: ["React", "HTML/CSS", "JavaScript"],
+  list: ["React", "JavaScript", "HTML/CSS"],
   icon: {IoGameController}
 }
 const graphicSkills=  {
   name: "Graphic Design",
-  list: ["Photoshop", "Figma", "Lightroom", "Procreate", "Adobe Illustrator"]
+  list: ["Procreate", "Adobe Illustrator"]
+}
+const infrastructureSkills=  {
+  name: "Tools & Infrastructure",
+  list: ["AWS", "CI/CD & Jenkins", "Git"]
 }
   
- 
 
-const skillsBackendData= [
-
-  {
-    name: 'Java',
-    level: 60,
-    color: 'gray',
-
-  },
-
-
-]
-
-const skillsFrontendData= [
-  {
-    name: 'JavaScript',
-    level: 40,
-    color: 'rgba(133, 114, 161, 1)',
-
-  },
-  {
-    name: 'ReactJS',
-    level: 50,
-    color: 'rgba(133, 114, 161, 1)',
-
-  },
-  {
-    name: 'HTML/SCSS',
-    level: 85,
-    color: 'rgba(133, 114, 161, 1)',
-
-  }
-
-]
 
 
 const Skills = () => {
@@ -88,11 +48,6 @@ const Skills = () => {
     viewport={{once: true, amount: 0.3}}
     >
       <h1 className='skills__title'>Skills</h1>
-    
-
- 
-
-
       <div className='app__skills_section'>
 
 
@@ -106,16 +61,16 @@ const Skills = () => {
 <SkillOverview skills = {frontendSkills}/>
 </div>
 <div className='skill-column'>
-<div className='skill-title'> <div><IoColorPalette className='title-icon'></IoColorPalette></div><h2> Graphic Design</h2></div>
+<h2 className='skill-title'><IoSettingsSharp/> Tools & Infrastructure</h2>
+<SkillOverview skills = {infrastructureSkills}/>
+</div>
+<div className='skill-column'>
+<h2  className='skill-title'><IoColorPalette/> Graphic Design</h2>
 <SkillOverview skills = {graphicSkills}/>
 </div>
+</div>
 
-
-      </div>
-
-
-    </motion.div>
-
+</motion.div>
   )
 }
 
